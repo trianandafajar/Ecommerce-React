dffccf44c5d090c12a600a32943bf893284f5f94
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-23 23:51:08 +0700

    Update Login

--- src/pages/Logout/index.js
+++ src/pages/Logout/index.js
@@ -1,32 +1,30 @@
 import * as React from 'react'; 
-import { useHistory} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { LayoutOne } from 'upkit';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import BounceLoader from 'react-spinners/BounceLoader';
 
 import { userLogout } from '../../features/Auth/actions';
 import { logout } from '../../api/auth';
 
-export default function Logout(){
+export default function Logout() {
+  const navigate = useNavigate(); // Ganti useHistory() dengan useNavigate()
+  const dispatch = useDispatch();
 
-  let history = useHistory();
-  let dispatch = useDispatch();
+  React.useEffect(() => {
+    logout()
+      .then(() => dispatch(userLogout()))
+      .then(() => navigate('/')); // Ganti history.push dengan navigate()
 
-   React.useEffect(() => {
+  }, [navigate, dispatch]);
 
-      logout()
-       .then(() => dispatch(userLogout()))
-       .then(() => history.push('/'));
-
-   }, [history, dispatch]);
-
-   return (
-     <LayoutOne size="small">
-       <div className="text-center flex flex-col justify-center items-center">
-         <BounceLoader color="red"/>
-         <br/>
-         Logging out ...
-       </div>
-     </LayoutOne>
-   )
+  return (
+    <LayoutOne size="small">
+      <div className="text-center flex flex-col justify-center items-center">
+        <BounceLoader color="red"/>
+        <br/>
+        Logging out ...
+      </div>
+    </LayoutOne>
+  );
 }
