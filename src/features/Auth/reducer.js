821dcf3669bb4b307b842f070d954cd0b9ceac51
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-29 21:03:23 +0700

    Update Auth Reducer

--- src/features/Auth/reducer.js
+++ src/features/Auth/reducer.js
@@ -1,21 +1,27 @@
-import { USER_LOGIN, USER_LOGOUT } from './constants';
+import { USER_LOGIN, USER_LOGOUT } from "./constants";
 
-let initialState = 
-	 localStorage.getItem('auth')
-   ? JSON.parse(localStorage.getItem('auth'))
-   : {user: null, token: null}
+const initialState = {
+  user: null,
+  token: null,
+};
 
-export default function reducer(state = initialState, action){
-
-  switch(action.type){
-
-    case USER_LOGIN: 
-      return { user: action.user, token: action.token }   
+export default function authReducer(state = initialState, action) {
+  switch (action.type) {
+    case USER_LOGIN:
+      return {
+        ...state,
+        user: action.user,
+        token: action.token,
+      };
 
     case USER_LOGOUT:
-      return { user: null, token: null}
-  
-      default:
-        return state;
-    }
+      return {
+        ...state,
+        user: null,
+        token: null,
+      };
+
+    default:
+      return state;
+  }
 }
