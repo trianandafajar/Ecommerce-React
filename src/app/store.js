c53014786a0f297544226a4da8b07947c5832291
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-29 21:03:35 +0700

    Update Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,19 +1,24 @@
-import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import authReducer from '../features/Auth/reducer';
 import productReducer from '../features/Products/reducer';
 import cartReducer from '../features/Cart/reducer';
 
-import thunk from 'redux-thunk';
+// Gunakan Redux DevTools jika tersedia di browser
+const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
-
-const rootReducers = combineReducers({
+// Gabungkan semua reducer
+const rootReducer = combineReducers({
   auth: authReducer,
   cart: cartReducer,
-  products: productReducer
+  products: productReducer,
 });
 
-const store = createStore(rootReducers, composerEnhancer(applyMiddleware(thunk)));
+// Buat store dengan middleware thunk
+const store = createStore(
+  rootReducer,
+  composerEnhancer(applyMiddleware(thunk))
+);
 
-export default store
+export default store;
