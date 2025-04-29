9fff2744e16a02eb0b230a21acf55311a736ab36
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-29 21:01:43 +0700

    Update Auth Actions

--- src/features/Auth/actions.js
+++ src/features/Auth/actions.js
@@ -1,18 +1,18 @@
-// (1) import constant
+// (1) import constants
 import { USER_LOGIN, USER_LOGOUT } from "./constants";
 
-// (2) action userLogin 
-export function userLogin(user, token){
+// (2) action untuk login user
+export function userLogin(user, token) {
   return {
     type: USER_LOGIN,
     user,
-    token
-  }
+    token,
+  };
 }
 
-// (3) action userLogout
-export function userLogout(){
+// (3) action untuk logout user
+export function userLogout() {
   return {
-    type: USER_LOGOUT
-  }
+    type: USER_LOGOUT,
+  };
 }
